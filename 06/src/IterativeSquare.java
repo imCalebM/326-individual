b2f75325657e470b8dfdd81dc3cdcb3599254ed1
Rhianne <price.rhianne@gmail.com>
2017-05-01 12:57:27 +1200

    comments fpr fractal squares

--- 06/src/IterativeSquare.java
+++ 06/src/IterativeSquare.java
@@ -1,48 +1,88 @@
+/**
+ * COSC 326
+ * Etude 6
+ * Fractal Squares
+ *
+ * IterativeSquare.java
+ *
+ * @author Rhianne Price
+ */
+
 import java.io.*;
 import java.awt.*;
 import java.awt.image.*;
 import javax.imageio.*;
 import javax.swing.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.*;
 
+/**
+ * A representation of a square with functionality to divide itself square into
+ * smaller squares, fill itself, and scale and save images.
+ */
 public class IterativeSquare {
   private int generation;
   private int x;
   private int y;
   private int size;
-  private IterativeSquare[] squares;
 
+  /**
+   * Constructor sets the position of the top left corner of the square and
+   * the size.
+   * @param x x position of the top left corner.
+   * @param y y position of the top left corner.
+   * @param size width/height of the square in pixels.
+   */
   public IterativeSquare(int x, int y, int size){
     this.x = x;
     this.y = y;
     this.size = size;
   }
 
-  public void subdivideAndFill(Graphics g){
+  /**
+   * Splits this square into 9 uniform squares, and fills the one in the centre.
+   * @param g graphics object to draw to.
+   * @return an array of 9 smaller squares.
+   */
+  public ArrayList<IterativeSquare> subdivideAndFill(Graphics g){
     int newSize = size / 3;
-    squares = new IterativeSquare [9];
+    ArrayList<IterativeSquare> squares; squares = new ArrayList<IterativeSquare>();
     for(int i = 0; i < 9; i ++){
-      squares[i] = new IterativeSquare(x + (i % 3) * newSize, y + (i / 3) * newSize, newSize);
+      squares.add(new IterativeSquare(x + (i % 3) * newSize, y + (i / 3) * newSize, newSize));
     }
     g.setColor(Color.red);
-    g.fillRect(squares[4].x, squares[4].y, squares[4].size, squares[4].size);
+    IterativeSquare middle = squares.get(4);
+    g.fillRect(middle.x, middle.y, middle.size, middle.size);
+    squares.remove(4);
+    return squares;
   }
 
-  public BufferedImage scaleToScreen(BufferedImage original) {
+  /**
+   * Creates a square buffered image the same size as the smaller dimension of
+   * the users display and draws the orignal image to it.
+   * @param original buffered image to be scaled.
+   * @return the scaled image.
+   */
+  public static BufferedImage scaleToScreen(BufferedImage original) {
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     double width = screenSize.getWidth();
     double height = screenSize.getHeight();
-    int largerEdge = height < width ? (int) width : (int) height;
+    int smallerEdge = height > width ? (int) width : (int) height;
 
-    BufferedImage scaledImage = new BufferedImage(largerEdge, largerEdge, BufferedImage.TYPE_BYTE_INDEXED);
+    BufferedImage scaledImage = new BufferedImage(smallerEdge, smallerEdge, BufferedImage.TYPE_BYTE_INDEXED);
     Graphics g = scaledImage.createGraphics();
-    g.drawImage(original, 0, 0, largerEdge, largerEdge, null);
+    g.drawImage(original, 0, 0, smallerEdge, smallerEdge, null);
     g.dispose();
 
     return scaledImage;
   }
 
-  public void saveImage(BufferedImage image, String name) {
+  /**
+   * Saves a buffered image as a PNG.
+   * @param image buffered image to be saved.
+   * @param name filename to write the file to.
+   */
+  public static void saveImage(BufferedImage image, String name) {
     try {
       File outputFile = new File(name);
       ImageIO.write(image, "png", outputFile);
@@ -53,10 +93,21 @@ public class IterativeSquare {
     }
   }
 
+  /**
+   * Creates a new square the size of the most squares that will fill a row,
+   * and a new buffered image of the same dimensions. It uses a queue to
+   * iteratively split the square into smaller squares, and fills the
+   * corrosponding area on the buffered image. The final image is then scaled
+   * and saved.
+   *
+   * @param args the first aragument specifies the number of generations of
+   *             squares to draw.
+   */
   public static void main(String[] args){
     int iterations = Integer.parseInt(args[0]);
     double size = (int) Math.pow(3, iterations + 1);
 
+    /* Create image and paint it white. */
     BufferedImage output = new BufferedImage((int) size, (int) size, BufferedImage.TYPE_BYTE_INDEXED);
     Graphics2D g = output.createGraphics();
     g.setColor(Color.white);
@@ -67,15 +118,12 @@ public class IterativeSquare {
     ConcurrentLinkedQueue<IterativeSquare> q = new ConcurrentLinkedQueue<IterativeSquare>();
     q.add(new IterativeSquare(0, 0, (int) size));
 
+    /* Subdivide and fill each square until the size of the squares gets down
+       to one pixel. */
     while(!q.isEmpty()) {
       IterativeSquare currentSquare = q.poll();
-      currentSquare.subdivideAndFill(g);
       if(currentSquare.size > 1) {
-        for(int i = 0; i < 9; i++) {
-          if(i != 4) {
-            q.add(currentSquare.squares[i]);
-          }
-        }
+          q.addAll(currentSquare.subdivideAndFill(g));
       }
     }
     g.dispose();
--- 06/src/RecursiveSquare.java
+++ 06/src/RecursiveSquare.java
@@ -1,9 +1,22 @@
+/**
+ * COSC 326
+ * Etude 6
+ * Fractal Squares
+ *
+ * RecursiveSquare.java
+ *
+ * @author Rhianne Price
+ */
 import java.io.*;
 import java.awt.*;
 import java.awt.image.*;
 import javax.imageio.*;
 import javax.swing.*;
 
+/**
+ * A recursive representation of a square. A square is made up of 9 smaller
+ * squares, can fill itself, and scale and save images.
+ */
 public class RecursiveSquare {
   private int generation;
   private int x;
@@ -11,6 +24,13 @@ public class RecursiveSquare {
   private int size;
   private RecursiveSquare[] squares;
 
+  /**
+   * Constructor sets the position of the top left corner of the square and
+   * the size.
+   * @param x x position of the top left corner.
+   * @param y y position of the top left corner.
+   * @param size width/height of the square in pixels.
+   */
   public RecursiveSquare(int generation, int x, int y, int size){
     this.generation = generation;
     this.x = x;
@@ -18,6 +38,10 @@ public class RecursiveSquare {
     this.size = size;
   }
 
+  /**
+   * Splits this square into 9 uniform squares, and fills the one in the centre.
+   * @param g graphics object to draw to.
+   */
   public void subdivideAndFill(Graphics g, int iterations) {
     if(generation == iterations + 1 || size == 0) return;
     int newSize = size / 3;
@@ -34,21 +58,32 @@ public class RecursiveSquare {
     }
   }
 
-  public BufferedImage scaleToScreen(BufferedImage original) {
+  /**
+   * Creates a square buffered image the same size as the smaller dimension of
+   * the users display and draws the orignal image to it.
+   * @param original buffered image to be scaled.
+   * @return the scaled image.
+   */
+  public static BufferedImage scaleToScreen(BufferedImage original) {
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     double width = screenSize.getWidth();
     double height = screenSize.getHeight();
-    int largerEdge = height < width ? (int) width : (int) height;
+    int smallerEdge = height > width ? (int) width : (int) height;
 
-    BufferedImage scaledImage = new BufferedImage(largerEdge, largerEdge, BufferedImage.TYPE_BYTE_INDEXED);
+    BufferedImage scaledImage = new BufferedImage(smallerEdge, smallerEdge, BufferedImage.TYPE_BYTE_INDEXED);
     Graphics g = scaledImage.createGraphics();
-    g.drawImage(original, 0, 0, largerEdge, largerEdge, null);
+    g.drawImage(original, 0, 0, smallerEdge, smallerEdge, null);
     g.dispose();
 
     return scaledImage;
   }
 
-  public void saveImage(BufferedImage image, String name) {
+  /**
+   * Saves a buffered image as a PNG.
+   * @param image buffered image to be saved.
+   * @param name filename to write the file to.
+   */
+  public static void saveImage(BufferedImage image, String name) {
     try {
       File outputFile = new File(name);
       ImageIO.write(image, "png", outputFile);
@@ -59,6 +94,16 @@ public class RecursiveSquare {
     }
   }
 
+  /**
+   * Creates a new square the size of the most squares that will fill a row
+   * depending on the number of generations to be drawn, and a new buffered
+   * image of the same dimensions. The square then recursively splits itself
+   * into smaller squares and fills the corrosponding areas on the buffered
+   * image. The final image is then scaled and saved.
+   *
+   * @param args the first aragument specifies the number of generations of
+   *             squares to draw.
+   */
   public static void main(String[] args){
     int iterations = Integer.parseInt(args[0]);
     double size = (int) Math.pow(3, iterations + 1);
@@ -71,7 +116,7 @@ public class RecursiveSquare {
     System.out.println("Drawing squares...");
 
     new RecursiveSquare(0, 0, 0, (int) size).subdivideAndFill(g, iterations);
-    
+
     g.dispose();
 
     saveImage(scaleToScreen(output), "fractal_squares_" + iterations + ".png");
